8cfe6f7721eeda08ecf73ac6c497118b63c3fad0
devhan-abdu <hanabduman@gmail.com>
2025-11-26 11:24:35 +0300

    fit: add useSession hook

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,21 +4,53 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/src/hooks/use-color-scheme';
+import { useEffect } from 'react';
+import { AppState } from 'react-native';
+import { AuthContextProvider, useSession } from '../hooks/useSession';
+import { supabase } from '../server/supabase';
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
 export default function RootLayout() {
+    return (
+        <AuthContextProvider>
+          <RootLayoutNav />
+        </AuthContextProvider>
+    )
+}
+
+function RootLayoutNav() {
   const colorScheme = useColorScheme();
+  const { session } = useSession()
+
+  useEffect(() => {
+      const subscription = AppState.addEventListener("change", (state) => {
+        if(state === 'active') {
+           supabase.auth.startAutoRefresh()
+        } else {
+           supabase.auth.stopAutoRefresh()
+        }
+      })
+  
+      return () => subscription.remove();
+  } , []
+)
+
 
   return (
+    
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <AuthContextProvider>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+       {
+        session ? (<Stack.Screen name="(tabs)" options={{ headerShown: false }} />) : (<Stack.Screen name="(auth)" options={{ headerShown: false }}/>)
+       }  
+        {/* // <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} /> */}
       </Stack>
       <StatusBar style="auto" />
+      </AuthContextProvider>
     </ThemeProvider>
   );
 }
---
+++ src/hooks/useSession.tsx
@@ -0,0 +1,47 @@
+import { Session } from "@supabase/supabase-js"
+import { createContext, ReactNode, useContext, useEffect, useMemo, useState } from "react"
+import { supabase } from "../server/supabase"
+
+
+type AuthContextType = {
+  session: Session | null,
+  loading: boolean,
+}
+
+
+export const AuthContext = createContext<AuthContextType | undefined>(undefined)
+
+
+export const AuthContextProvider = ({children}: {children: ReactNode})=> {
+    const  [session,setSession] = useState<Session |null >(null)
+    const [loading , setLoading] = useState<boolean>(true) 
+
+
+    useEffect(()=> {
+      supabase.auth.getSession().then(({data: {session}}) => {
+        setSession(session)
+      }).finally(() =>  setLoading(false))
+
+
+        const {data: {subscription}} = supabase.auth.onAuthStateChange((_event, session) => {
+           setSession(session)
+            setLoading(false)
+        })
+
+
+      return () => subscription.unsubscribe()
+    } , [])
+
+    const value =useMemo(() => ({session, loading}), [session, loading])
+
+     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+
+}
+
+export const useSession = () => {
+  const context =  useContext(AuthContext) 
+  if (!context) {
+    throw new Error("useSession must be used within an AuthContextProvider")
+  }
+  return {session : context.session, user: context?.session?.user, loading: context.loading}
+}
