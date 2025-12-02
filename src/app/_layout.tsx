654c619eed96aa677382d4eeb464a8859eee6727
devhan-abdu <hanabduman@gmail.com>
2025-12-02 10:26:09 +0300

    feat(app): add _layout for tab navigati

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,56 +1,57 @@
+import { useColorScheme } from '@/src/hooks/use-color-scheme';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/src/hooks/use-color-scheme';
 import { useEffect } from 'react';
-import { AppState } from 'react-native';
+import { ActivityIndicator, AppState, View } from 'react-native';
+import 'react-native-reanimated';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AuthContextProvider, useSession } from '../hooks/useSession';
 import { supabase } from '../server/supabase';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import "./../global.css";
 
 export default function RootLayout() {
-    return (
-        <AuthContextProvider>
-          <RootLayoutNav />
-        </AuthContextProvider>
-    )
-}
-
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-  const { session } = useSession()
-
-  useEffect(() => {
-      const subscription = AppState.addEventListener("change", (state) => {
-        if(state === 'active') {
-           supabase.auth.startAutoRefresh()
-        } else {
-           supabase.auth.stopAutoRefresh()
-        }
-      })
-  
-      return () => subscription.remove();
-  } , []
-)
-
+    const colorScheme = useColorScheme();
 
   return (
-    
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AuthContextProvider>
-      <Stack>
-       {
-        session ? (<Stack.Screen name="(tabs)" options={{ headerShown: false }} />) : (<Stack.Screen name="(auth)" options={{ headerShown: false }}/>)
-       }  
-        {/* // <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} /> */}
-      </Stack>
+    <AuthContextProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <SafeAreaProvider className='flex-1'>
+      <RootLayoutNav />
+      </SafeAreaProvider>
       <StatusBar style="auto" />
-      </AuthContextProvider>
     </ThemeProvider>
+    </AuthContextProvider>
+  );
+}
+
+
+
+function RootLayoutNav() {
+  const { session, loading } = useSession();
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener("change", (state) => {
+      supabase.auth[state === 'active' ? 'startAutoRefresh' : 'stopAutoRefresh']();
+    });
+    return () => subscription.remove();
+  }, []);
+
+  if (loading) {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      {session ? (
+        <Stack.Screen name="(auth)" />
+      ) : (
+        <Stack.Screen name="(app)/index" />
+      )}
+    </Stack>
   );
 }
