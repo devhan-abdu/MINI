ab10dc3f49fd2d7aac17500232bd559653cc2eae
devhan-abdu <hanabduman@gmail.com>
2025-12-03 11:18:48 +0300

    fit: add StatusBadge

---
+++ src/components/ui/StatusBadge.tsx
@@ -0,0 +1,77 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Text, View } from "react-native";
+
+type Status = "Completed" | "Pending" | "Missed" | "Partial";
+
+interface StatusBadgeProps {
+    status: Status,
+}
+
+export default function StatusBadge({status}: StatusBadgeProps) {
+
+  const getStatusStyle= (status: Status) => {
+     switch(status) {
+        case "Completed": 
+        return {
+            bg:"bg-green-50",
+            text:"text-primary",
+            border: "border-green-200",
+            icon: 'checkmark-circle',
+            iconColor: "#666"
+        };
+
+          case "Pending": 
+        return {
+            bg:"bg-amber-50",
+            text:"text-amber-800",
+            border: "border-amber-200",
+            icon: 'checkmark-circle',
+            iconColor: "#666"
+        };
+
+          case "Missed": 
+        return {
+            bg:"bg-red-50",
+            text:"text-red-800",
+            border: "border-red-200",
+            icon: 'checkmark-circle',
+            iconColor: "#666"
+        };
+
+          case "Partial": 
+        return {
+            bg:"bg-yellow-50",
+            text:"text-yellow-800",
+            border: "border-yellow-200",
+            icon: 'checkmark-circle',
+            iconColor: "#666"
+        };
+           default:
+        return {
+            bg:"bg-gray-50",
+            text:"text-gray-800",
+            border: "border-gray-200",
+            icon: 'checkmark-circle',
+            iconColor: "#666"
+        };
+
+     }
+  };
+
+  const style = getStatusStyle(status)
+
+  return (
+    <View className={`flex-row items-center rounded-full px-3 py-1.5 border ${style.bg} ${style.border} `}>
+        <Ionicons
+         name={style.icon as any}
+         size={14}
+         color={style.iconColor}
+         style = {{marginRight: 4}} />
+
+         <Text className={`font-medium text-sm ${style.text}`}>
+          {status}
+         </Text>
+    </View>
+  )
+
+}
