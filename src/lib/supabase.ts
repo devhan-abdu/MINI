cbb90ee04cd141f5b16f151010f8db6515c74b0f
devhan-abdu <hanabduman@gmail.com>
2025-12-09 21:34:53 +0300

    fit: move supabse.ts to lib

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient, processLock } from '@supabase/supabase-js';
+import 'expo-sqlite/localStorage/install';
+import { AppState, Platform } from 'react-native';
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabasePublishableKey =process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+    lock: processLock,
+  },
+});
+
+if(Platform.OS !== "web") {
+  AppState.addEventListener('change', (state) => {
+           if(state === 'active') {
+            supabase.auth.startAutoRefresh()
+           } else {
+             supabase.auth.stopAutoRefresh()
+           }
+  })
+}
