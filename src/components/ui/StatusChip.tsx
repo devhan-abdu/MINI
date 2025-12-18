5b0014697fbc988cb3ce184b5c4fc7dfba2168c0
devhan-abdu <hanabduman@gmail.com>
2025-12-18 20:56:33 +0300

    fit: add StatusChip

---
+++ src/components/ui/StatusChip.tsx
@@ -0,0 +1,39 @@
+import { Pressable, Text, View } from "react-native";
+
+export const StatusChip = ({
+  status,
+}: {
+  status: "pending" | "completed" | "partial" | "missed";
+}) => {
+  console.log(status, "statussss");
+  const config = {
+    completed: {
+      label: "Completed",
+      bg: "bg-green-100",
+      text: "text-green-700",
+    },
+    partial: {
+      label: "Partial",
+      bg: "bg-yellow-100",
+      text: "text-yellow-700",
+    },
+    missed: {
+      label: "Missed",
+      bg: "bg-red-100",
+      text: "text-red-700",
+    },
+    pending: {
+      label: "Partial",
+      bg: "bg-yellow-100",
+      text: "text-yellow-700",
+    },
+  };
+
+  const s = config[status];
+
+  return (
+    <View className={`${s.bg} px-4 py-2  self-start`}>
+      <Text className={`font-semibold ${s.text}`}>{s.label}</Text>
+    </View>
+  );
+};
