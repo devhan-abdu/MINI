9850f32452f085d4692b12b26f3e2bbad7eedd26
devhan-abdu <hanabduman@gmail.com>
2025-12-19 20:38:09 +0300

    fit: integrate backend

--- src/app/(app)/muraja/weekly-plane/index.tsx
+++ src/app/(app)/muraja/weekly-plane/index.tsx
@@ -1,128 +1,121 @@
 import ScreenWrapper from "@/src/components/ScreenWrapper";
 import WeeklyPlanCard from "@/src/components/WeeklyPlaneCard";
+import { useSession } from "@/src/hooks/useSession";
+import { useWeeklyDays } from "@/src/hooks/useWeeklyDays";
+import { useWeeklyPlan } from "@/src/hooks/useWeeklyPlan";
 import { useRouter } from "expo-router";
-import { useState } from "react";
 import { Text, View } from "react-native";
+import { fullDayNames } from "@/src/lib/utils";
+import { TodaySkeleton } from "@/src/components/skeletons";
 
+export default function WeeklyPlan() {
+  const router = useRouter();
+  const { user } = useSession();
+  const { weeklyPlan } = useWeeklyPlan(user?.id ?? null);
+  const { plans, todayPlan, upcomingSessions, loading } = useWeeklyDays(
+    user?.id ?? null,
+    weeklyPlan?.id ?? null
+  );
 
-  const mockWeekData = [
-  {
-    id: "1",
-    day: "Monday",
-    date: "Dec 16",
-    status: "Completed" as const,
-    juz: "Juz 3-4",
-    pages: 20,
-    minutes: 45,
-  },
-  {
-    id: "2",
-    day: "Tuesday",
-    date: "Dec 17",
-    status: "Partial" as const,
-    juz: "Juz 5",
-    pages: 25,
-    minutes: 60,
-  },
-  {
-    id: "3",
-    day: "Wednesday",
-    date: "Dec 18",
-    status: "Pending" as const,
-    juz: "Juz 6",
-    pages: 22,
-    minutes: 50,
-  },
-  {
-    id: "4",
-    day: "Thursday",
-    date: "Dec 19",
-    status: "Pending" as const,
-    juz: "Juz 7",
-    pages: 18,
-    minutes: 40,
-  },
-  {
-    id: "5",
-    day: "Friday",
-    date: "Dec 20",
-    status: "Missed" as const,
-    juz: "Juz 8",
-    pages: 20,
-    minutes: 45,
-  },
-  {
-    id: "6",
-    day: "Saturday",
-    date: "Dec 21",
-    status: "Pending" as const,
-    juz: "Juz 9",
-    pages: 30,
-    minutes: 75,
-  },
-];
+  if (!weeklyPlan || !plans) return null;
 
-export default function CreatePlane() {
-
-    const router = useRouter()
-    const [plans, setPlans] = useState(mockWeekData);
-    
-
-
-
-  const handleCardPress = (id: string) => {
-    console.log("Card pressed:", id);
-    // Navigate to log page or show modal
-  };
-
-  const handleLogMuraja = (id: string) => {
-    console.log("Log Muraja'a for:", id);
-    // Update status to "Completed" or "Partial"
-    setPlans(prev =>
-      prev.map(plan =>
-        plan.id === id
-          ? { ...plan, status: "Completed" }
-          : plan
-      )
-    );
-  };
+  const today = new Date().toISOString().slice(0, 10);
+  const pastPlans = plans.filter((p) => p.date < today && p.date !== today);
+  const completedPlanCount = plans.filter(
+    (p) => p.status === "completed"
+  ).length;
+  const progressPercentage = Math.min(
+    100,
+    Math.max(0, (completedPlanCount / plans.length) * 100)
+  );
 
   return (
-   <ScreenWrapper>
-     <View className="bg-white p-4 rounded-lg border border-gray-200 flex-col gap-4 mb-12">
-       <Text className="text-semibold text-lg">
-          This Week's Progress
-       </Text>
-         <View className={`overflow-hidden rounded-full h-4 bg-gray-400 w-full`} >
-         <View className="h-full rounded-full"
-          style={{
-          width: `${Math.min(100, Math.max(0, 65))}%`,
-          backgroundColor: "#276359"
-         }}
-         >
-
-         </View>
-         </View>
-       <Text>{2} of {3} Completed</Text>
-     </View>
-   {mockWeekData.map((plan) => (
-        <WeeklyPlanCard
-          key={plan.id}
-          day={`${plan.day} • ${plan.date}`}
-          status={plan.status}
-          juz={plan.juz}
-          pages={plan.pages}
-          minutes={plan.minutes}
-          onPress={() => 
-            router.push(`/muraja/weekly-plane/${plan.id}`)
-            // plan.status === "Completed" 
-            //   ? handleCardPress(plan.id)
-            //   : handleLogMuraja(plan.id)
-
-          }
-        />
-      ))}
-    
-   </ScreenWrapper>
+    <ScreenWrapper>
+      <View className="bg-white p-4 rounded-lg border border-gray-200 flex-col gap-4 mb-12">
+        <Text className="text-semibold text-lg">This Week's Progress</Text>
+        <View className={`overflow-hidden rounded-full h-4 bg-gray-400 w-full`}>
+          <View
+            className="h-full rounded-full"
+            style={{
+              width: `${progressPercentage}%`,
+              backgroundColor: "#276359",
+            }}
+          ></View>
+        </View>
+        <Text>
+          {completedPlanCount} of {plans.length} Days Completed
+        </Text>
+      </View>
+      {loading ? (
+        <>
+          {[1, 2, 3, 4, 5].map((i) => (
+            <TodaySkeleton key={i} />
+          ))}
+        </>
+      ) : (
+        <>
+          {todayPlan && (
+            <>
+              <Text className="text-2xl font-bold mb-4">Today</Text>
+              <WeeklyPlanCard
+                planId={todayPlan.id ?? null}
+                day={fullDayNames[todayPlan.day_of_week]}
+                date={todayPlan.date}
+                status={todayPlan.status}
+                startSurah={todayPlan.startSurah}
+                endSurah={todayPlan.endSurah}
+                logId={todayPlan.log_id}
+                pages={weeklyPlan?.planned_pages}
+                minutes={weeklyPlan?.estimated_time_min}
+                startPage={todayPlan.planned_start_page}
+                endPage={todayPlan.planned_end_page}
+              />
+            </>
+          )}
+          {pastPlans.length > 0 && (
+            <>
+              <Text className="text-2xl font-bold my-4">Past</Text>
+              {pastPlans.map((plan) => (
+                <WeeklyPlanCard
+                  key={plan.id}
+                  planId={plan.id ?? null}
+                  day={fullDayNames[plan.day_of_week]}
+                  date={plan.date}
+                  status={plan.status}
+                  startSurah={plan.startSurah}
+                  endSurah={plan.endSurah}
+                  logId={plan.log_id}
+                  pages={weeklyPlan?.planned_pages}
+                  minutes={weeklyPlan?.estimated_time_min}
+                  startPage={plan.planned_start_page}
+                  endPage={plan.planned_end_page}
+                />
+              ))}
+            </>
+          )}
+          {upcomingSessions.length > 0 && (
+            <>
+              <Text className="text-2xl font-bold my-4">Upcoming</Text>
+              {upcomingSessions.map((plan) => (
+                <WeeklyPlanCard
+                  key={plan.id}
+                  planId={plan.id ?? null}
+                  day={fullDayNames[plan.day_of_week]}
+                  date={plan.date}
+                  status={plan.status}
+                  startSurah={plan.startSurah}
+                  endSurah={plan.endSurah}
+                  logId={null}
+                  pages={weeklyPlan?.planned_pages}
+                  minutes={weeklyPlan?.estimated_time_min}
+                  startPage={plan.planned_start_page}
+                  endPage={plan.planned_end_page}
+                />
+              ))}
+            </>
+          )}
+        </>
+      )}
+    </ScreenWrapper>
   );
 }
--- src/components/WeeklyPlaneCard.tsx
+++ src/components/WeeklyPlaneCard.tsx
@@ -2,64 +2,81 @@ import { Ionicons } from "@expo/vector-icons";
 import { Text, View } from "react-native";
 import { Button } from "./ui/Button";
 import StatusBadge from "./ui/StatusBadge";
-type StatusType = "Completed" | "Pending" | "Missed" | "Partial";
+import { useRouter } from "expo-router";
+
+type StatusType = "completed" | "pending" | "missed" | "partial";
 
 interface WeeklyPlanCardProps {
   day: string;
+  date: string;
   status: StatusType;
-  juz: string;
+  startSurah: string;
+  endSurah: string;
   pages: number;
   minutes: number;
-  onPress: () => void;
+  logId: number | null;
+  startPage: number;
+  endPage: number;
+  planId: number | null;
 }
 
 export default function WeeklyPlanCard({
   day,
+  date,
   status,
-  juz,
+  startSurah,
+  endSurah,
+  logId,
+  planId,
   pages,
   minutes,
-  onPress
+  startPage,
+  endPage,
 }: WeeklyPlanCardProps) {
+  const router = useRouter();
+  const today = new Date().toISOString().slice(0, 10);
+  if (!planId) return null;
+
+  const handlePress = () => {
+    router.push({
+      pathname: "/muraja/weekly-plane/[id]",
+      params: {
+        id: planId,
+        status: status,
+        logId: logId,
+      },
+    });
+  };
   return (
-    <View className="border border-gray-300 bg-white shadow-sm rounded-xl flex-col gap-4 px-4 py-5 mb-4">
-      <View className="flex-row justify-between items-center">
+    <View
+      className="border border-gray-300 bg-white shadow-sm rounded-xl flex-col gap-4 px-4 py-5 mb-4"
+      key={planId}
+    >
+      <View className="flex-row justify-between items-center mb-2">
         <View>
-          <Text className="text-xl font-bold text-gray-900">
-            {day}
-          </Text>
-          <Text className="text-sm text-gray-500 mt-1">
-            Daily reading goal
+          <Text className="text-xl font-bold text-gray-900">{day}</Text>
+          <Text className="text-sm text-gray-900 mt-1">
+            {startSurah === endSurah
+              ? startSurah
+              : `${startSurah} – ${endSurah}`}
           </Text>
         </View>
-        <StatusBadge status={status}  />
+        <StatusBadge status={status} />
       </View>
-      
-      <View className="flex-row items-center justify-between px-2">
+
+      <View className="flex-row items-center justify-between px-2 border-t border-gray-200 pt-4">
         <View className="items-center flex-1">
           <View className="flex-row items-center gap-2 mb-1">
             <Ionicons name="book-outline" size={16} color="#4b5563" />
             <Text className="text-sm text-gray-700 font-medium">
-              {juz}
+              page {startPage} - {endPage}
             </Text>
           </View>
-          <Text className="text-xs text-gray-500">Juz</Text>
+          <Text className="text-xs text-gray-500">Page</Text>
         </View>
-        
+
         <View className="h-6 w-px bg-gray-300" />
-        
-        <View className="items-center flex-1">
-          <View className="flex-row items-center gap-2 mb-1">
-            <Ionicons name="layers-outline" size={16} color="#4b5563" />
-            <Text className="text-sm text-gray-700 font-medium">
-              {pages} pages
-            </Text>
-          </View>
-          <Text className="text-xs text-gray-500">Pages</Text>
-        </View>
-        
-        <View className="h-6 w-px bg-gray-300" />
-        
+
         <View className="items-center flex-1">
           <View className="flex-row items-center gap-2 mb-1">
             <Ionicons name="time-outline" size={16} color="#4b5563" />
@@ -69,15 +86,32 @@ export default function WeeklyPlanCard({
           </View>
           <Text className="text-xs text-gray-500">Time</Text>
         </View>
+
+        <View className="h-6 w-px bg-gray-300" />
+        <View className="items-center flex-1">
+          <View className="flex-row items-center gap-2 mb-1">
+            <Ionicons name="layers-outline" size={16} color="#4b5563" />
+            <Text className="text-sm text-gray-700 font-medium">
+              {pages} pages
+            </Text>
+          </View>
+          <Text className="text-xs text-gray-500">Pages</Text>
+        </View>
       </View>
 
-      <Button 
-        className="py-3 mt-4"
-        onPress={onPress}
-        variant={status === "Completed" ? "outline" : "primary"}
-      >
-        {status === "Completed" ? "View Log" : "Log Today's Muraja'a"}
-      </Button>
+      {date <= today && (
+        <Button
+          className="py-3 mt-4"
+          onPress={handlePress}
+          variant={today === date ? "primary" : "outline"}
+        >
+          {today === date
+            ? "Log Today's Muraja'a"
+            : status === "completed"
+            ? "Veiw Log"
+            : "Log Past Day"}
+        </Button>
+      )}
     </View>
   );
 }
