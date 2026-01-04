498298b62d6f4d3d97a276b3f59c04f3af4c8fd5
devhan-abdu <hanabduman@gmail.com>
2026-01-04 23:05:26 +0300

    feat(hifz): integrate create hifz plan with backend

--- src/app/(app)/create-hifz-plan.tsx
+++ src/app/(app)/create-hifz-plan.tsx
@@ -1,14 +1,7 @@
 import ScreenWrapper from "@/src/components/ScreenWrapper";
 import { Ionicons } from "@expo/vector-icons";
 import { Stack, useRouter } from "expo-router";
-import {
-  Alert,
-  Pressable,
-  Text,
-  TextInput,
-  View,
-  ScrollView,
-} from "react-native";
+import { Alert, Pressable, Text, View } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import Input from "@/src/components/ui/Input";
 import SurahDropdown, {
@@ -19,14 +12,20 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { useState } from "react";
 import { Button } from "@/src/components/ui/Button";
 import { useSession } from "@/src/hooks/useSession";
-import { useLoadSurahData } from "@/src/hooks/useFetchQuran";
-import { HifzPlanSchema } from "@/src/features/hifz/types";
+import {
+  HifzPlanSchema,
+  HifzPlanSchemaFormType,
+  IHifzPlan,
+} from "@/src/features/hifz/types";
+import SelectDays from "@/src/features/muraja/components/SelectDays";
+import { hifzServices } from "@/src/features/hifz/services/hifz";
+import { calculatePlanStats } from "@/src/features/hifz/utils";
+import StatsSummary from "@/src/features/hifz/components/StatsSummary";
 
 export default function CreateHifzPlan() {
   const router = useRouter();
   const [showDatePicker, setShowDatePicker] = useState(false);
   const { user } = useSession();
-  const { items } = useLoadSurahData();
 
   const isCreating = false;
 
@@ -34,226 +33,247 @@ export default function CreateHifzPlan() {
     handleSubmit,
     formState: { errors },
     control,
-    setValue,
   } = useForm({
     resolver: yupResolver(HifzPlanSchema),
     defaultValues: {
       start_date: new Date().toISOString().split("T")[0],
-      days_per_week: 5,
-      pages_per_day: 1,
+      selectedDays: [],
+      pages_per_day: 2,
       start_surah: 1,
       start_page: 1,
       direction: "forward",
-      note: "",
     },
   });
 
-  const formData = useWatch({ control });
-
-  const calculateStats = () => {
-    const totalQuranPages = 604;
-    const startPage = formData.start_page || 1;
-
-    const totalPages =
-      formData.direction === "forward"
-        ? totalQuranPages - startPage + 1
-        : startPage;
-
-    const dailyRate = formData.pages_per_day || 1;
-    const weeklyFreq = formData.days_per_week || 5;
-
-    const daysNeeded = Math.ceil((totalPages / dailyRate) * (7 / weeklyFreq));
-
-    const finishDate = new Date(formData.start_date || new Date());
-    finishDate.setDate(finishDate.getDate() + daysNeeded);
-
-    return { totalPages, finishDate, daysNeeded };
-  };
-
-  const stats = calculateStats();
-
-  const onSubmit = async (data: any) => {
+  const onSubmit = async (data: HifzPlanSchemaFormType) => {
     if (!user?.id) return;
-    console.log("Hifz Plan Data:", { ...data, ...stats });
+
+    try {
+      const stats = calculatePlanStats(data);
+      const { selectedDays, ...rest } = data;
+
+      const planData: Omit<IHifzPlan, "hifz_daily_logs"> = {
+        ...rest,
+        user_id: user.id,
+        selected_days: selectedDays,
+        total_pages: stats.totalPages,
+        estimated_end_date: stats.finishDate.toISOString().slice(0, 10),
+        status: "active",
+        days_per_week: data.selectedDays.length,
+      };
+
+      await hifzServices.createPlan({ planData });
+      Alert.alert("Success", "Hifz plan created successfully!", [
+        {
+          text: "OK",
+          onPress: () => router.back(),
+        },
+      ]);
+    } catch (error: any) {
+      Alert.alert(` ${error.message}`);
+    }
   };
+
+  const startSurah = useWatch({ control, name: "start_surah" });
 
   return (
     <>
       <Stack.Screen
-        options={{ title: "New Hifz Journey", headerShown: true }}
+        options={{
+          title: "New Hifz Journey",
+          headerShown: true,
+          headerShadowVisible: true,
+          headerStyle: { backgroundColor: "#fff" },
+          headerTintColor: "#111827",
+          headerTitleStyle: { fontWeight: "600" },
+        }}
       />
+
       <ScreenWrapper>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View className="flex-row bg-gray-100 p-1 rounded-2xl mb-6">
-            {["forward", "backward"].map((dir) => (
-              <Pressable
-                key={dir}
-                onPress={() => setValue("direction", dir as any)}
-                className={`flex-1 py-3 rounded-xl items-center ${
-                  formData.direction === dir ? "bg-white shadow-sm" : ""
-                }`}
-              >
-                <Text
-                  className={`font-bold ${
-                    formData.direction === dir
-                      ? "text-primary-700"
-                      : "text-gray-500"
-                  }`}
-                >
-                  {dir === "forward" ? "Traditional" : "Reverse (Juz Amma)"}
-                </Text>
-              </Pressable>
-            ))}
-          </View>
-
-          <View className="bg-primary-900 p-6 rounded-3xl mb-8 shadow-xl">
-            <Text className="text-primary-100/70 text-xs uppercase font-bold tracking-widest mb-1">
-              Estimated Finish Date
-            </Text>
-            <Text className="text-white text-3xl font-black mb-4">
-              {stats.finishDate.toLocaleDateString("en-US", {
-                month: "long",
-                day: "numeric",
-                year: "numeric",
-              })}
-            </Text>
-
-            <View className="flex-row border-t border-white/10 pt-4 justify-between">
-              <View>
-                <Text className="text-primary-100/50 text-xs font-bold uppercase">
-                  Total Pages
-                </Text>
-                <Text className="text-white text-lg font-bold">
-                  {stats.totalPages} Pages
-                </Text>
-              </View>
-              <View className="items-end">
-                <Text className="text-primary-100/50 text-xs font-bold uppercase">
-                  Journey Length
-                </Text>
-                <Text className="text-white text-lg font-bold">
-                  ~{Math.round(stats.daysNeeded / 30)} Months
-                </Text>
-              </View>
-            </View>
-          </View>
-
-          <View className="space-y-6">
-            <View>
-              <Text className="text-gray-500 font-bold mb-2 ml-1">
-                START DATE
-              </Text>
-              <Controller
-                control={control}
-                name="start_date"
-                render={({ field: { value, onChange } }) => (
+        <View>
+          <Controller
+            control={control}
+            name="direction"
+            render={({ field: { value, onChange } }) => (
+              <View className="mb-6">
+                <View className="flex-row items-center justify-center bg-gray-100/70 p-1.5 rounded-full border border-gray-200/40">
                   <Pressable
-                    onPress={() => setShowDatePicker(true)}
-                    className="bg-white border border-gray-200 p-4 rounded-2xl flex-row justify-between"
+                    onPress={() => {
+                      console.log("Setting forward");
+                      onChange("forward");
+                    }}
+                    className={`
+          px-6 py-3 rounded-full mx-1 w-1/2 max-w-54
+          transition-all duration-300 ease-in-out
+          ${value === "forward" ? "bg-primary " : "bg-transparent"}
+        `}
                   >
-                    <Text className="text-base font-medium">
-                      {new Date(value).toDateString()}
+                    <Text
+                      className={`
+            font-semibold text-base tracking-wide text-center
+            ${value === "forward" ? "text-white" : "text-gray-600"}
+          `}
+                    >
+                      Traditional
                     </Text>
-                    <Ionicons name="calendar" size={20} color="#276359" />
-                    {showDatePicker && (
-                      <DateTimePicker
-                        value={new Date(value)}
-                        onChange={(e, d) => {
-                          setShowDatePicker(false);
-                          if (d) onChange(d.toISOString().split("T")[0]);
-                        }}
-                      />
-                    )}
                   </Pressable>
-                )}
-              />
-            </View>
 
-            <View className="flex-row gap-4">
-              <Controller
-                control={control}
-                name="pages_per_day"
-                render={({ field: { value, onChange } }) => (
-                  <Input
-                    label="PAGES / DAY"
-                    value={String(value)}
-                    setValue={(v) => onChange(Number(v))}
-                    keyboardType="numeric"
-                    containerClassName="flex-1"
-                  />
-                )}
-              />
-              <Controller
-                control={control}
-                name="days_per_week"
-                render={({ field: { value, onChange } }) => (
-                  <Input
-                    label="DAYS / WEEK"
-                    value={String(value)}
-                    setValue={(v) => onChange(Number(v))}
-                    keyboardType="numeric"
-                    containerClassName="flex-1"
-                  />
-                )}
-              />
-            </View>
-
-            <View className="flex-row gap-4">
-              <View className="flex-1">
-                <Controller
-                  control={control}
-                  name="start_surah"
-                  render={({ field: { value, onChange } }) => (
-                    <SurahDropdown surah={value} setSurah={onChange} />
-                  )}
-                />
+                  <Pressable
+                    onPress={() => {
+                      console.log("Setting backward");
+                      onChange("backward");
+                    }}
+                    className={`
+          px-6 py-3 rounded-full mx-1 w-1/2  max-w-54
+          transition-all duration-300 ease-in-out
+          ${value === "backward" ? "bg-primary " : "bg-transparent"}
+        `}
+                  >
+                    <Text
+                      className={`
+            font-semibold text-base tracking-wide text-center
+            ${value === "backward" ? "text-white" : "text-gray-600"}
+          `}
+                    >
+                      Reverse Juz Amma
+                    </Text>
+                  </Pressable>
+                </View>
               </View>
-              <View className="flex-1">
-                <Controller
-                  control={control}
-                  name="start_page"
-                  render={({ field: { value, onChange } }) => (
-                    <SurahPageDropdown
-                      surah={formData.start_surah ?? null}
-                      page={value}
-                      setPage={onChange}
-                    />
-                  )}
-                />
-              </View>
-            </View>
+            )}
+          />
+          {errors.direction && (
+            <Text className="text-sm text-red-500">
+              {errors.direction.message}
+            </Text>
+          )}
+        </View>
 
-            <View>
-              <Text className="text-gray-500 font-bold mb-2 ml-1">NOTES</Text>
-              <Controller
-                control={control}
-                name="note"
-                render={({ field: { value, onChange } }) => (
-                  <TextInput
-                    value={value ?? ""}
-                    onChangeText={onChange}
-                    placeholder="E.g. Focusing on Juz Amma first..."
-                    multiline
-                    className="bg-white border border-gray-200 p-4 rounded-2xl min-h-[100px]"
-                    textAlignVertical="top"
+        <StatsSummary control={control} />
+
+        <View className="mb-6">
+          <Text className="text-gray-900 font-semibold mb-2.5">START DATE</Text>
+          <Controller
+            control={control}
+            name="start_date"
+            render={({ field: { value, onChange } }) => (
+              <Pressable
+                onPress={() => setShowDatePicker(true)}
+                className="bg-white border border-gray-200 p-4 rounded-2xl flex-row justify-between"
+              >
+                <Text className="text-base font-medium">
+                  {new Date(value).toDateString()}
+                </Text>
+                <Ionicons name="calendar" size={20} color="#276359" />
+                {showDatePicker && (
+                  <DateTimePicker
+                    value={new Date(value)}
+                    onChange={(e, d) => {
+                      setShowDatePicker(false);
+                      if (d) onChange(d.toISOString().split("T")[0]);
+                    }}
                   />
                 )}
-              />
-            </View>
-          </View>
+              </Pressable>
+            )}
+          />
+          {errors.start_date && (
+            <Text className="text-sm text-red-500">
+              {errors.start_date.message}
+            </Text>
+          )}
+        </View>
 
-          <View className="mt-10 mb-20">
-            <Button
-              onPress={handleSubmit(onSubmit)}
-              disabled={isCreating}
-              className="h-16 rounded-2xl shadow-lg"
-            >
-              <Text className="text-white font-bold text-lg">
-                Start Journey
+        <View className="mb-6">
+          <Controller
+            control={control}
+            name="pages_per_day"
+            render={({ field: { value, onChange } }) => (
+              <Input
+                label="PAGES / DAY"
+                value={String(value)}
+                setValue={(v) => onChange(Number(v))}
+                keyboardType="numeric"
+                containerClassName="flex-1"
+              />
+            )}
+          />
+          {errors.pages_per_day && (
+            <Text className="text-sm text-red-500">
+              {errors.pages_per_day.message}
+            </Text>
+          )}
+        </View>
+
+        <View className="flex-row gap-4">
+          <View className="flex-1">
+            <Text className="text-gray-900 font-semibold mb-2.5 uppercase">
+              Start Surah
+            </Text>
+            <Controller
+              control={control}
+              name="start_surah"
+              render={({ field: { value, onChange } }) => (
+                <SurahDropdown surah={value} setSurah={onChange} />
+              )}
+            />
+            {errors.start_surah && (
+              <Text className="text-sm text-red-500">
+                {errors.start_surah.message}
               </Text>
-            </Button>
+            )}
           </View>
-        </ScrollView>
+          <View className="flex-1">
+            <Text className="text-gray-900 font-semibold mb-2.5 uppercase">
+              Page num
+            </Text>
+            <Controller
+              control={control}
+              name="start_page"
+              render={({ field: { value, onChange } }) => (
+                <SurahPageDropdown
+                  surah={startSurah}
+                  page={value}
+                  setPage={onChange}
+                />
+              )}
+            />
+            {errors.start_page && (
+              <Text className="text-sm text-red-500">
+                {errors.start_page.message}
+              </Text>
+            )}
+          </View>
+        </View>
+
+        <View className="mb-6">
+          <Text className="text-gray-900 font-semibold mb-2.5 uppercase">
+            Select Days
+          </Text>
+          <Text className="text-gray-500 text-sm mb-4">
+            Choose which days you want Hifz
+          </Text>
+          <Controller
+            name="selectedDays"
+            control={control}
+            render={({ field: { value, onChange } }) => (
+              <SelectDays value={value ?? []} onChange={onChange} />
+            )}
+          />
+          {errors.selectedDays && (
+            <Text className="text-red-500">{errors.selectedDays.message}</Text>
+          )}
+        </View>
+
+        <View className="mt-10 mb-20">
+          <Button
+            onPress={handleSubmit(onSubmit)}
+            disabled={isCreating}
+            className=" rounded-2xl shadow-lg"
+          >
+            <Text className="text-white font-bold text-lg">Start Journey</Text>
+          </Button>
+        </View>
       </ScreenWrapper>
     </>
   );
