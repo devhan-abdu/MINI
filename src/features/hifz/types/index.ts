29a711b0f63d0ad2e7bd71105b86f0ea0da6a40a
devhan-abdu <hanabduman@gmail.com>
2026-01-09 08:53:11 +0300

    feat: add Hifz types

--- src/features/hifz/types/index.ts
+++ src/features/hifz/types/index.ts
@@ -1,44 +1,52 @@
 export interface IHifzLog {
     id?: number;
-    hifz_plan_day_id: number;
+    hifz_plan_id: number;
     actual_start_page: number;
     actual_end_page: number;
-    completed_pages: number;
-    status: "completed" | "partial" | "missed",
-    notes: string;
-};
-
-export interface IHifzPlanDays {
-    id?: number;
-    hifz_plan_id: number;
+    actual_pages_completed: number;
     date: string;
-    planned_surah: string;
-    planned_end_page: number;
-    planned_pages: number;
-    day_of_week: string;
-
-    is_active: boolean;
-    hifz_daily_logs?: IHifzLog[]
+  log_day: number;
+    status: "completed" | "partial" | "missed",
+    notes?: string;
 };
+
+
 
 export interface IHifzPlan {
+  user_id?: string,
   id?: number;                 
-
-  start_surah: string;
+  start_surah: number;
   start_page: number;
-
-  days_per_week: number;
-  target_end_date: string | null; 
-  duration: number | null;
-
   total_pages: number;
   pages_per_day: number;
-  pages_per_week: number;
-
+  selected_days: number[];
+  days_per_week: number
   start_date: string;         
   estimated_end_date: string;  
-
-    status: "active" | "completed" | "paused";
-    HIFZ_PLAN_DAYS?: IHifzPlanDays[];
+  direction: 'forward' | "backward", 
+  status?: "active" | "completed" | "paused";
+  hifz_daily_logs?: IHifzLog[]
     
 }
+
+import * as Yup from "yup";
+
+export const HifzPlanSchema = Yup.object({
+  start_date: Yup.string().required("Start date is required"),
+  start_surah: Yup.number().required("Start surah is required").min(1).max(114),
+  start_page: Yup.number().required("Start page is required").min(1).max(604),
+  
+  direction: Yup.string().oneOf(['forward', 'backward']).required().default("forward"),
+  selectedDays: Yup.array()
+   .of(Yup.number().required())
+   .min(1, "select at least one day")
+   .required("please select days"),
+    
+  pages_per_day: Yup.number()
+    .required("Pages per day required")
+    .min(0.1, "Minimum 0.1 page") 
+    .typeError("Must be a number"),
+      
+});
+
+export type HifzPlanSchemaFormType = Yup.InferType<typeof HifzPlanSchema>;
