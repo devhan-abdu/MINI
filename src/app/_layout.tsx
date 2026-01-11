31c9e122a529069a228c6c0e3b736e90c63498c8
devhan-abdu <hanabduman@gmail.com>
2026-01-11 14:44:46 +0300

    chore: refresh pnpm-lock to resolve build cache issues

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,14 +2,16 @@ import { AuthContextProvider } from "../hooks/useSession";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./../global.css";
 import { RootLayoutNav } from "../components/navigation/RootLayoutNav";
-import { SplashScreen } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { useFonts } from "expo-font";
 
-export default function RootLayout() {
-  const queryClient = new QueryClient();
+  SplashScreen.preventAutoHideAsync().catch(() => {});
+    const queryClient = new QueryClient();
 
-  SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+
   const [loaded, error] = useFonts({
     Rosemary: require("../../assets/fonts/Rosemary.ttf"),
   });
