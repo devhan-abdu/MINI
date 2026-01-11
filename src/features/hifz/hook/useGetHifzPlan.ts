6959ec8a3f7fd98a8720636ba6bedf609e4171e2
devhan-abdu <hanabduman@gmail.com>
2026-01-11 13:02:34 +0300

    feat(hifz): implement nextTask calculation

--- src/features/hifz/hook/useGetHifzPlan.ts
+++ src/features/hifz/hook/useGetHifzPlan.ts
@@ -1,11 +1,15 @@
 import { useSession } from "@/src/hooks/useSession"
 import { useQuery } from "@tanstack/react-query";
 import { hifzServices } from "../services/hifz";
+import { useMemo } from "react";
+import { useLoadSurahData } from "@/src/hooks/useFetchQuran";
+import { getNextTask } from "../utils/quran-logic";
 
 export const useGetHifzPlan = () => {
-    const { user } = useSession();
+    const { user } = useSession()
+    const {items: surahData} = useLoadSurahData()
 
-    const { data, isLoading, isError, refetch } = useQuery({
+    const { data: hifz, isLoading, isError, refetch } = useQuery({
         queryKey: ["hifz", user?.id],
         queryFn: () => {
             if (!user?.id) return null;
@@ -14,8 +18,36 @@ export const useGetHifzPlan = () => {
         enabled: !!user?.id 
     })
 
+    const nextTask = useMemo(() => {
+        if (!hifz || !surahData) return null;
+        
+           const logs = hifz.hifz_daily_logs || [];
+           const lastLog = logs[logs.length - 1]
+           const referencePage = lastLog ? lastLog.actual_end_page  : hifz.start_page
+           
+          
+           const nextTask = getNextTask(
+             hifz.direction,
+             referencePage,
+             hifz.pages_per_day,
+             surahData,
+             !lastLog
+           );
+        
+           if (!nextTask) return null;
+        
+           return {
+             ...nextTask,
+             surah: nextTask.displaySurah, 
+             isBackward: hifz.direction === "backward",
+               target: hifz.pages_per_day,
+             status: lastLog?.status 
+           };
+    },[hifz, surahData])
+
     return {
-        hifz: data,
+        nextTask,
+        hifz,
         isLoading,
         error: isError,
         refetch
