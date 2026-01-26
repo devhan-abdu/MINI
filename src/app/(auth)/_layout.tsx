486118634f7fb74c4f9a32180f5e0e19aff483b0
devhan-abdu <hanabduman@gmail.com>
2026-01-26 16:14:59 +0300

    feat: add auth screen animations

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -5,9 +5,23 @@ export default function AuthLayout() {
     <Stack
       screenOptions={{
         headerShown: false,
+        animation: "fade",
+        animationDuration: 400,
       }}
     >
-      <Stack.Screen name="login" />
+      <Stack.Screen name="intro" />
+      <Stack.Screen
+        name="login"
+        options={{
+          animation: "slide_from_bottom",
+        }}
+      />
+      <Stack.Screen
+        name="register"
+        options={{
+          animation: "slide_from_bottom",
+        }}
+      />
     </Stack>
   );
 }
